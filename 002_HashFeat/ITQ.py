83d5789dfc38c81a51e2a8830315e097653d7d01
Rohit Girdhar <rgirdhar@andrew.cmu.edu>
2016-01-22 14:03:39 -0500

    custom PCA

--- 002_HashFeat/ITQ.py
+++ 002_HashFeat/ITQ.py
@@ -1,13 +1,9 @@
 import numpy as np
-from sklearn.decomposition import PCA
+from scipy import linalg as LA
 
 def train(feats, nbits, NITER=50):
-  pcaComputer = PCA(n_components=nbits)
   print('Computing PCA')
-  pcaComputer.fit(feats)
-  feats = pcaComputer.transform(feats)  # can also be done by np.dot(feats, pcaComputer.components_)
-  pc = pcaComputer.components_.transpose()
-  mean = pcaComputer.mean_
+  feats, pc, mean = PCA(feats, nbits)
   
   rot_matrix = np.random.randn(nbits, nbits)
   u_matrix, _, _ = np.linalg.svd(rot_matrix)
@@ -24,4 +20,33 @@ def train(feats, nbits, NITER=50):
     print('Distance of new matrix: %f' % 
         np.linalg.norm(np.matrix(rot_matrix_old) - np.matrix(rot_matrix)))
   return (mean, pc, rot_matrix)
+
+def PCA(data, dims_rescaled_data=2):
+  # from http://stackoverflow.com/a/13224592/1492614
+  """
+  returns: data transformed in 2 dims/columns + regenerated original data
+  pass in: data as 2D NumPy array
+  """
+  m, n = data.shape
+  # mean center the data
+  mean_ = np.mean(data, axis=0)
+  data -= mean_
+  # calculate the covariance matrix
+  R = np.cov(data, rowvar=False)
+  # calculate eigenvectors & eigenvalues of the covariance matrix
+  # use 'eigh' rather than 'eig' since R is symmetric, 
+  # the performance gain is substantial
+  evals, evecs = LA.eigh(R)
+  # sort eigenvalue in decreasing order
+  idx = np.argsort(evals)[::-1]
+  evecs = evecs[:,idx]
+  # sort eigenvectors according to same index
+  evals = evals[idx]
+  # select the first n eigenvectors (n is desired dimension
+  # of rescaled data array, or dims_rescaled_data)
+  evecs = evecs[:, :dims_rescaled_data]
+  # carry out the transformation on the data using eigenvectors
+  # and return the re-scaled data, eigenvalues, and eigenvectors
+  #return np.dot(evecs.T, data.T).T, eigenvalues, eigenvectors
+  return np.dot(data, evecs), evecs, mean_
 
