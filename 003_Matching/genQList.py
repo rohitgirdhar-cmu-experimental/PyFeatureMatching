bbae19e91a8b5dff35ef7c606eaa166dffa3931c
Rohit Girdhar <rgirdhar@andrew.cmu.edu>
2016-02-07 13:38:49 -0500

    running more matching

--- 003_Matching/genQList.py
+++ 003_Matching/genQList.py
@@ -3,6 +3,7 @@ import numpy as np
 scoresfpath = '/home/rgirdhar/Work/Data/014_TVShows/processed/Scratch/002_FriendsDVD/007_SceneClassScore/002_PlaceNetFT/iter_7K/AllScores.txt'
 imgslistfpath = '/home/rgirdhar/Work/Data/014_TVShows/processed/Lists/friends/AllFrames.txt'
 outfpath = '/home/rgirdhar/Work/Data/014_TVShows/processed/Scratch/002_FriendsDVD/007_SceneClassScore/002_PlaceNetFT/iter_7K/Queries.txt'
+nquery = 20000
 
 with open(imgslistfpath) as f:
   imgslist = f.read().splitlines()
@@ -11,4 +12,4 @@ with open(scoresfpath) as f:
 
 order = np.argsort(-np.array(scoreslist))  # for 1-index output
 imgslist = np.array(imgslist)
-np.savetxt(outfpath, imgslist[order[:10000]], delimiter='\n', fmt='%s')
+np.savetxt(outfpath, imgslist[order[:nquery]], delimiter='\n', fmt='%s')
